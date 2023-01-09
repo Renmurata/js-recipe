2ff1ac1462ae588c9c7434f8ada0126075ba30d4
Renmurata <tmjuit@gmail.com>
2023-01-09 15:15:41 +0900

    [カウンター]を学習しました。

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,4 +1,5 @@
 // 引数 number を受け取る関数
+genkiFunction(24)
 const genkiFunction = function(number) {
     // n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
     for (let n = 1; n <= number; n++) {
